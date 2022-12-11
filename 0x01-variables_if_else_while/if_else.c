2cf2792184590ad252b59efc81cbd80b4ee07988
HARON MURUMBA <haron.murumba22@gmail.com>
2022-12-11 07:23:23 -0800

    this code cant run

---
+++ 0x01-variables_if_else_while/if_else.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main- if if else statement
+* return : always 0
+*/
+exter x
+int main(void)
+{
+	int x = 100;
+
+	if
+	(x == 5);
+	{
+		printf("iam balanced\n,"x);
+	}
+	else
+	{
+		printf("am overloaded");
+	}
+
+	return (0);
+}
