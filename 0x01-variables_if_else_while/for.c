f283c2aba3e6ef90058c128fc232316d2faf1180
HARON MURUMBA <haron.murumba22@gmail.com>
2022-12-12 01:25:32 -0800

    for statement

---
+++ 0x01-variables_if_else_while/for.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i <= 30; i++)
+	{
+		printf("yes haron");
+	}
+
+	for (int k = 0; k <= 10; k++)
+	{
+		printf("NUM %d", k);
+	}
+	return (0);
+}
