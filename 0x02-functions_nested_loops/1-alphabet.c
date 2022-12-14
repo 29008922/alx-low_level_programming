f74f7ff0598504ff1b1675c60dfd023023eb82fc
HARON MURUMBA <haron.murumba22@gmail.com>
2022-12-13 22:18:03 -0800

    o-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints the lowercase alphabet
+*
+* Return- void
+*/
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la == "a..z")
+	{
+		_putchar(la);
+	}
+	_putchar('\n');
+}
