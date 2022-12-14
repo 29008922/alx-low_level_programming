c59e8caea81b889cf4997637b8c948eabc7d58ef
HARON MURUMBA <haron.murumba22@gmail.com>
2022-12-14 08:47:11 -0800

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,7 @@
 
 void positvi_or_negative(int i);
 int largest_number(int a, int b, int c);
-int print_remaining_days(int day int month int year);
-int convert_day(int month int day);
+int print_remaining_days(int day, int month, int year);
+int convert_day(int month, int day);
+
 #endif
