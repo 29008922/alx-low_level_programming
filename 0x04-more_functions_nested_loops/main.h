1307ec183f7f591e3265e1bb62f22e3f0ee3be2b
HARON MURUMBA <haron.murumba22@gmail.com>
2022-12-15 08:37:52 -0800

    task0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H
-#define MAIN.H
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,6 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_number(int n);
 void print_triangle(int size);
-
-
-#endif
