a3e9d0226a057bf671608e723f3d8e898aa2622d
HARON MURUMBA <haron.murumba22@gmail.com>
2022-12-19 02:51:14 -0800

    task0

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main: set n to 98
+*@n: input
+*return:sucess 1:wq
+*/
+
+int main(void)
+{
+	int n;
+	int *p;
+
+	*p = &n
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
-*reset_to_98 - update the value of n to 98
-*@n: input
-*return:value of n
+* reset_to_98 - updates the value of the variable
+* the pointer points to to 98
+* @n: pointer to the variable to update
 */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return (n);
 }
