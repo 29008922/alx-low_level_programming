4fe160293f8c8da6b51af8f35ea2f899f3b1e513
HARON MURUMBA <haron.murumba22@gmail.com>
2023-01-03 04:46:02 -0800

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strpbrk - prints the consecutive character os s1 that are in s2.
+* @s: source string
+* @accept: searching string
+*
+* Return: returns new string.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + 1); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + 1) == *(accept + j))
+			{
+				break;
+			}
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
+		}
+	}
+	return (0);
+}
