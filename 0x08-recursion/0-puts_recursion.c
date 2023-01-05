845102ab7a08fea7bfe7b1b2852d58c24c1101ee
HARON MURUMBA <haron.murumba22@gmail.com>
2023-01-05 05:59:23 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+/**
+ *file:0-puts_recursion.c
+ *author: HARON
+ */
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: pointer to the char array which is the string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
