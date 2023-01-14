552176dd9c15cabad8024ec475372181815f2abb
HARON MURUMBA <haron.murumba22@gmail.com>
2023-01-14 04:11:21 -0800

    task0

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,14 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
-  *_isalpha - controls if a character is alphabetical
-  *@c: character to be verified
-  *Return: return 0 or 1
-  */
-int _isalpha(int c)
+char *_strpbrk(char *s, char *accept)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (1);
-
-	return (0);
+	return strpbrk(s, accept);
 }
