efce7b2d96604a3c512d3b28099e6110112e21e3
HARON MURUMBA <haron.murumba22@gmail.com>
2023-01-18 05:30:43 -0800

    alpha practice

---
+++ practice_repo/alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* prgram to print alphabet*/
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		printf("%c\n", alphabet);
+		alphabet++;
+	}
+	return 0;
+}
