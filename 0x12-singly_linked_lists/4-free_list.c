0beb27dd208fb3bdcaecf87d1c6ba301e08620fa
HARON MURUMBA <haron.murumba22@gmail.com>
2023-01-30 09:52:08 -0800

    commit for singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list.
+ * @head: list to be freed.
+ *
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t tmp;
+
+	if (head == NULL)
+		return;
+		while (head != NULL)
+		{
+			tmp = head->next;
+			free(head->str);
+			free(head);
+			head = tmp;										}
+}
