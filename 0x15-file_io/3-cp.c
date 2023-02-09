72c83380a6b9f13c7ad985ef58695e8fc61a1b4c
HARON MURUMBA <haron.murumba22@gmail.com>
2023-02-09 10:14:55 -0800

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * error_file - checks if files can be opened.
@@ -7,7 +8,6 @@
  * @argv: arguments vector.
  * Return: no return.
  */
-
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -27,12 +27,11 @@ void error_file(int file_from, int file_to, char *argv[])
  * @argv: arguments vector.
  * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	ssize_t count, temp;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -42,19 +41,19 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
-	nchars = 1024;
 
-	while (nchars == 1024)
+	count = 1024;
+	while (count == 1024)
 	{
-		nchars = read(file_from, buf, 1024)
-		if (nchars == -1)
+		count = read(file_from, buffer, 1024);
+		if (count == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
+		temp = write(file_to, buffer, count);
+				if (temp == -1)
+					error_file(0, -1, argv);
 	}
 	err_close = close(file_from);
-	if (err_close == -)
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
@@ -63,8 +62,7 @@ int main(int argc, char *argv[])
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	{
-		return (0);
+	exit(100);
+	}
+	return (0);
 }
-
