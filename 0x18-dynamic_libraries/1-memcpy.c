e908631514bc77eec680f7bd327d7304e470b9ef
HARON <haron.murumba22@gmail.com>
2023-03-21 02:10:15 -0700

    shared library

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - irontnoux2
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			dest[i] = src[i];
+	}
+	return (dest);
+}
