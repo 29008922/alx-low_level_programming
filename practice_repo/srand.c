5339a16ff1dbba01d849669f42bd73e5088183b8
HARON MURUMBA <haron.murumba22@gmail.com>
2023-07-02 02:30:21 -0700

    puts

--- practice_repo/srand.c
+++ practice_repo/srand.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(){
-    unsigned int seed;
-    int n, counter;
- 
-    printf("Enter a unsigned number(seed)\n");
-    scanf("%u", &seed);
-     
-    /* Initialize random number generator using srand */
-    srand(seed);
-     
-    printf("Enter number of random numbers\n");
-    scanf("%d", &n);
-    for(counter=0; counter < n; counter++){
-        printf("%d ", rand());
-    }
-     
-    return 0;
+
+int main(void)
+{
+	unsigned int seed;
+	int n, counter;
+
+	printf("Enter a unsigned number(seed)\n");
+	scanf("%u", &seed);
+	/* Initialize random number generator using srand */
+	srand(seed);
+
+	printf("Enter number of random numbers\n");
+	scanf("%d", &n);
+
+	for (counter = 0; counter < n; counter++)
+	{
+		printf("%d\n ", rand());
+	}
+	return (0);
 }
 
